66d8f7bec3286792e1b4b86313abbd8759f04319
alvinmrrry <alvin_mrrry@outlook.com>
2025-05-18 00:02:20 +0800

    app/todolist/page.tsx

---
+++ app/todolist/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import DynamicIslandTodo from "@/components/DynamicIslandTodo";
+
+export default function TodoPage() {
+  return (
+    <div>
+      <DynamicIslandTodo />
+    </div>
+  );
+}
